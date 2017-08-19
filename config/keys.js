8f0465f89251f897781497b75f64cea7181210fd
Ryan Heupler <ryan.heupler@inventivhealth.com>
2017-08-19 17:08:57 -0400

    adds passport dependency

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 // KEYS.JS
 if ( process.env.NODE_ENV === 'production' ) {
-  // we're in prod, return prod keys
+  // return prod keys
   module.exports = require('./prod');
 } else {
-  // we're in dev, return dev keys
+  // return dev keys
   module.exports = require('./dev');
 }
